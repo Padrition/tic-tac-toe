e6de60417a703a239f474d303d7634ca1d1913bc
padrition <padrition@gmail.com>
2021-03-22 02:33:39 +0600

    print a 3x3 board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    //print an empty board
+    //every iteration ask for a number of a cell player wants to place his sign to 
+    let mut cells = vec!['1','2','3','4','5','6','7','8','9'];
+    let mut index = 0;
+    for _ in 0..3{
+        for _ in 0..3{
+            print!("{:2}|", cells[index]);
+            index += 1;
+        }
+        print!("\n");
+    }
+
 }
