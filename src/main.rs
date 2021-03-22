d5cfe25b687efb83ededa29e984c755b911be588
padrition <padrition@gmail.com>
2021-03-23 01:21:04 +0600

    change of input sign

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,42 @@
 use std::io;
 
-struct Board{
-    board : Vec<char>,
+struct Board {
+    board: Vec<char>,
 }
 
-impl Board{
-    fn print_a_board(&self){
+impl Board {
+    fn print_a_board(&self) {
         let mut index = 0;
-        for _ in 0..3{
-            for _ in 0..3{
+        for _ in 0..3 {
+            for _ in 0..3 {
                 print!("{:2}", self.board[index]);
                 index += 1;
             }
-            print!("\n");
+            println!();
         }
     }
-    fn place_a_sign(&mut self,index: usize, sign : char){
-        self.board[index-1] = sign;
+    fn place_a_sign(&mut self, index: usize, sign: char) {
+        self.board[index - 1] = sign;
     }
-    fn new()-> Board{
-        Board{
-            board: vec!['\u{25A2}';9],
+    fn new() -> Board {
+        Board {
+            board: vec!['\u{25A2}'; 9],
         }
     }
 }
 
 fn main() {
     println!("\tTIC-TAC-TOE");
-    println!("The rulse:
+    println!(
+        "The rulse:
             \rEvery turn you asked where you want to place your sign(O or X).
-            \rIf you fill any row column or horizontal with your signs you win.");
+            \rIf you fill any row column or horizontal with your signs you win."
+    );
 
     let mut b = Board::new();
+    let mut sign: char = 'X';
 
-    loop{
+    loop {
         b.print_a_board();
 
         println!("Enter the number of a cell you want to sign:");
@@ -44,9 +47,9 @@ fn main() {
             .read_line(&mut cell_number)
             .expect("Faild to read the input.");
 
-        let index : usize;
+        let index: usize;
 
-        match cell_number.trim().parse(){
+        match cell_number.trim().parse() {
             Ok(i) => index = i,
             Err(_) => {
                 println!("Enter a valid number!");
@@ -54,7 +57,16 @@ fn main() {
             }
         }
 
-        b.place_a_sign(index, 'X');
+        match sign {
+            'X' => {
+                b.place_a_sign(index, sign);
+                sign = 'O';
+            }
+            'O' => {
+                b.place_a_sign(index, sign);
+                sign = 'X';
+            }
+            _ => panic!("How de fuck you managed to break it?"),
+        }
     }
-    
 }
