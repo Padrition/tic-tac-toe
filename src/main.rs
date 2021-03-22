f688ff99ee3a1f21434f6383d0c56de64401b040
padrition <padrition@gmail.com>
2021-03-22 18:36:37 +0600

    instead of numbers now board prints empty squares

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ impl Board{
         let mut index = 0;
         for _ in 0..3{
             for _ in 0..3{
-                print!("{:2}|", self.board[index]);
+                print!("{:2}", self.board[index]);
                 index += 1;
             }
             print!("\n");
@@ -23,15 +23,14 @@ impl Board{
     }
     fn new()-> Board{
         Board{
-            board: vec!['1','2','3','4','5','6','7','8','9']
+            board: vec!['\u{25A2}';9]
         }
     }
 }
 
 fn main() {
-    //print an empty board
-    //every iteration ask for a number of a cell player wants to place his sign to
     let mut b = Board::new();
     b.place_a_sign(Some(0), 'X');
     b.print_a_board();
+    println!("")
 }
