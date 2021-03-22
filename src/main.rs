4a006573233a5b68562503eac7b9def77d0c3dc7
padrition <padrition@gmail.com>
2021-03-22 18:04:08 +0600

    refactored process of changeing signs

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,18 @@ fn main() {
     //print an empty board
     //every iteration ask for a number of a cell player wants to place his sign to 
     let mut cells = vec!['1','2','3','4','5','6','7','8','9'];
-    print_a_board(&mut cells, Some(1), 'X');
+    place_a_sign(&mut cells, Some(0), 'X');
+    print_a_board(&mut cells);
 }
-
-fn print_a_board(board :&mut Vec<char>, option : Option<usize>, sign : char){
+fn place_a_sign(board :&mut Vec<char>, option : Option<usize>, sign : char){
     match option{
         Some(i) => {
             board[i] = sign;
         },
         None => println!("None"),
     }
+}
+fn print_a_board(board :&mut Vec<char>){
     let mut index = 0;
     for _ in 0..3{
         for _ in 0..3{
