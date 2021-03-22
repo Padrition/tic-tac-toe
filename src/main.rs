a3642e7e42e98f6b6c8d158fca35279adc3cd63e
padrition <padrition@gmail.com>
2021-03-22 18:30:43 +0600

    created a struct board and implemented print_a_board and place_a_sign for this struct

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,37 @@
+struct Board{
+    board : Vec<char>,
+}
+
+impl Board{
+    fn print_a_board(self){
+        let mut index = 0;
+        for _ in 0..3{
+            for _ in 0..3{
+                print!("{:2}|", self.board[index]);
+                index += 1;
+            }
+            print!("\n");
+        }
+    }
+    fn place_a_sign(&mut self, option : Option<usize>, sign : char){
+        match option{
+            Some(i) => {
+                self.board[i] = sign;
+            },
+            None => println!("None"),
+        }
+    }
+    fn new()-> Board{
+        Board{
+            board: vec!['1','2','3','4','5','6','7','8','9']
+        }
+    }
+}
+
 fn main() {
     //print an empty board
-    //every iteration ask for a number of a cell player wants to place his sign to 
-    let mut cells = vec!['1','2','3','4','5','6','7','8','9'];
-    place_a_sign(&mut cells, Some(0), 'X');
-    print_a_board(&mut cells);
-}
-fn place_a_sign(board :&mut Vec<char>, option : Option<usize>, sign : char){
-    match option{
-        Some(i) => {
-            board[i] = sign;
-        },
-        None => println!("None"),
-    }
-}
-fn print_a_board(board :&mut Vec<char>){
-    let mut index = 0;
-    for _ in 0..3{
-        for _ in 0..3{
-            print!("{:2}|", board[index]);
-            index += 1;
-        }
-        print!("\n");
-    }
+    //every iteration ask for a number of a cell player wants to place his sign to
+    let mut b = Board::new();
+    b.place_a_sign(Some(0), 'X');
+    b.print_a_board();
 }
