3e5dd454eed693b2620f1c0274328e47759e3ea5
padrition <padrition@gmail.com>
2021-03-22 20:05:39 +0600

    added a game loop

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::io;
+
 struct Board{
     board : Vec<char>,
 }
 
 impl Board{
-    fn print_a_board(self){
+    fn print_a_board(&self){
         let mut index = 0;
         for _ in 0..3{
             for _ in 0..3{
@@ -23,14 +25,36 @@ impl Board{
     }
     fn new()-> Board{
         Board{
-            board: vec!['\u{25A2}';9]
+            board: vec!['\u{25A2}';9],
         }
     }
 }
 
 fn main() {
+    println!("\tTIC-TAC-TOE");
+    println!("The rulse:
+            \rEvery turn you asked where you want to place your sign(O or X).
+            \rIf you fill any row column or horizontal with your signs you win.");
+
     let mut b = Board::new();
-    b.place_a_sign(Some(0), 'X');
-    b.print_a_board();
-    println!("")
+
+    loop{
+        b.print_a_board();
+
+        println!("Enter the number of a cell you want to sign:");
+
+        let mut cell_number = String::new();
+
+        io::stdin()
+            .read_line(&mut cell_number)
+            .expect("Faild to read the input.");
+
+        let index : usize = cell_number.trim().parse().expect("Faild to convert cell_number(Stirng) to index(usize)");
+
+        b.place_a_sign(Some(index), '\u{2716}');
+        //read the input for cell_number
+        //convert it to int index
+        //assign a char to index
+    }
+    
 }
